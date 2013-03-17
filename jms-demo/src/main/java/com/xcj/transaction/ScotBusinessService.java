dcddcc785930f92eead75a5776efa7fcce68587f
Martin1987 <xie.likai1987@gmail.com>
2013-03-17 19:37:49 +0800

    2013-3-18 transaction success

--- jms-demo/src/main/java/com/xcj/transaction/ScotBusinessService.java
+++ jms-demo/src/main/java/com/xcj/transaction/ScotBusinessService.java
@@ -3,26 +3,61 @@ package com.xcj.transaction;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.transaction.annotation.Transactional;
 
 public class ScotBusinessService
 {
     private DeptDAO deptDao;
 
-    private DeptpkDAO deptPkDao;
+    private DeptbakDAO deptbakDao;
 
     @Transactional(rollbackFor = SQLException.class)
     public void addDepartment(Dept dept)
         throws SQLException
     {
         deptDao.insert(dept);
-        DeptpkExample example = new DeptpkExample();
-        example.createCriteria().addCriterion("deptno=" + dept.getDeptno());
-        List<Deptpk> pks = deptPkDao.selectByExample(example);
-        if (pks == null || pks.isEmpty() || !pks.contains(dept.getDeptno()))
+        Dept dep = deptDao.selectByPrimaryKey(new Short("50"));
+        System.out.println(dep.getDname());
+        Deptbak bak = deptbakDao.selectByPrimaryKey(dept.getDeptno());
+        if (bak == null)
         {
             throw new SQLException();
         }
 
+    }
+    
+    public DeptDAO getDeptDao() {
+		return deptDao;
+	}
+
+	public void setDeptDao(DeptDAO deptDao) {
+		this.deptDao = deptDao;
+	}
+
+	public DeptbakDAO getDeptbakDao() {
+		return deptbakDao;
+	}
+
+	public void setDeptbakDao(DeptbakDAO deptbakDao) {
+		this.deptbakDao = deptbakDao;
+	}
+
+	public static void main(String[] args){
+    	ApplicationContext context = new ClassPathXmlApplicationContext("transaction-context.xml");
+    	Dept dept = new Dept();
+    	dept.setDeptno(new Short("50"));
+    	dept.setDname("test");
+    	dept.setLoc("testLoc");
+    	
+    	ScotBusinessService bs = (ScotBusinessService) context.getBean("scottBS");
+    	try {
+			bs.addDepartment(dept);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
 }
